1a461e35d10a9ac74ac607f4188c3508869450a9
hoho00 <jhoya963@gmail.com>
2023-09-10 19:20:19 +0900

    Feat: 버튼 만들기

---
+++ src/components/ButtonGithub.js
@@ -0,0 +1,13 @@
+const ButtonGithub = () => {
+  return (
+    <a
+      href="https://github.com/jongmin0919/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <button>Go to Github page </button>
+    </a>
+  );
+};
+
+export default ButtonGithub;
